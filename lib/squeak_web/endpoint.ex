16d0e24a95d5201c729e4c582b5b561c40c087a7
Dashie <dashie@sigpipe.me>
2020-05-26 21:53:50 +0200

    Add Pow

--- lib/squeak_web/endpoint.ex
+++ lib/squeak_web/endpoint.ex
@@ -43,5 +43,6 @@ defmodule SqueakWeb.Endpoint do
   plug Plug.MethodOverride
   plug Plug.Head
   plug Plug.Session, @session_options
+  plug Pow.Plug.Session, otp_app: :squeak
   plug SqueakWeb.Router
 end
--- lib/squeak_web/router.ex
+++ lib/squeak_web/router.ex
@@ -1,5 +1,6 @@
 defmodule SqueakWeb.Router do
   use SqueakWeb, :router
+  use Pow.Phoenix.Router
 
   pipeline :browser do
     plug :accepts, ["html"]
@@ -11,6 +12,12 @@ defmodule SqueakWeb.Router do
 
   pipeline :api do
     plug :accepts, ["json"]
+  end
+
+  scope "/" do
+    pipe_through :browser
+
+    pow_routes()
   end
 
   scope "/", SqueakWeb do

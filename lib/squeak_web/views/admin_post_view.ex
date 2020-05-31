67822bc1a2c6e203457330648fa63cecf743f611
Dashie <dashie@sigpipe.me>
2020-05-31 09:31:16 +0200

    Refactor view helpers

--- lib/squeak_web/views/admin_post_view.ex
+++ lib/squeak_web/views/admin_post_view.ex
@@ -1,13 +1,4 @@
 defmodule SqueakWeb.AdminPostView do
   use SqueakWeb, :view
-  use Timex
-
-  def date_format_iso(date) do
-    date_format(date, "{ISO:Extended:Z}")
-  end
-
-  def date_format(date, format_string \\ "{D} {Mshort} {YYYY}") do
-    date
-    |> Timex.format!(format_string)
-  end
+  import SqueakWeb.FormatterHelpers
 end

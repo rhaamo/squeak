e076ca98dd615a6d6dde29c9226b3a39b2126771
Dashie <dashie@sigpipe.me>
2020-06-18 09:07:31 +0200

    Try to add edit media

--- lib/squeak_web/controllers/admin_medias_controller.ex
+++ lib/squeak_web/controllers/admin_medias_controller.ex
@@ -50,10 +50,64 @@ defmodule SqueakWeb.AdminMediaController do
     |> redirect(to: SqueakWeb.Router.Helpers.admin_media_path(conn, :list))
   end
 
-  def edit(_conn, _params) do
+  def edit(conn, %{"id" => media_id}) do
+    media = Squeak.Repo.get_by(Squeak.Medias.Media, flake_id: media_id)
+
+    if is_nil(media) do
+      conn
+      |> put_flash(:error, "Media not found")
+      |> redirect(to: SqueakWeb.Router.Helpers.admin_media_path(conn, :list))
+    end
+
+    changeset = Squeak.Medias.Media.changeset(media, %{})
+    render(conn, "edit.html", changeset: changeset, media_id: media_id, media: media)
   end
 
-  def update(_conn, _params) do
+  def update(conn, %{"id" => media_id, "media" => media_params}) do
+    date = NaiveDateTime.truncate(NaiveDateTime.utc_now(), :second)
+
+    media = Squeak.Repo.get_by(Squeak.Medias.Media, flake_id: media_id)
+
+    if is_nil(media) do
+      conn
+      |> put_flash(:error, "Media not found")
+      |> redirect(to: SqueakWeb.Router.Helpers.admin_media_path(conn, :list))
+    end
+
+    params =
+      media_params
+      |> Map.put("updated_at", date)
+
+    if Map.has_key?(media_params, "media") do
+      params =
+        params
+        |> Map.put("original_filename", media_params["media"].filename)
+        |> Map.put("mime", media_params["media"].content_type)
+
+      changeset = Media.changeset(media, params)
+
+      {:ok, _media} =
+        Ecto.Multi.new()
+        |> Ecto.Multi.update(:media, changeset)
+        |> Ecto.Multi.update(:media_with_file, &Media.media_changeset(&1.media, params))
+        |> Squeak.Repo.transaction()
+
+      conn
+      |> put_flash(:info, "Media updated")
+      |> redirect(to: SqueakWeb.Router.Helpers.admin_media_path(conn, :show, media.flake_id))
+    else
+      changeset = Media.changeset(media, params)
+
+      if changeset.valid? do
+        {:ok, _obj} = Squeak.Repo.update(changeset)
+
+        conn
+        |> put_flash(:info, "Media updated")
+        |> redirect(to: SqueakWeb.Router.Helpers.admin_media_path(conn, :show, media.flake_id))
+      else
+        render(conn, "edit.html", changeset: %{changeset | action: :insert}, media_id: media_id)
+      end
+    end
   end
 
   def delete(conn, %{"id" => media_id}) do

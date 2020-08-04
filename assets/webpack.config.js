2bf9688b8a01f02fff30fa7f7edf60f8a0d2aa68
Dashie <dashie@sigpipe.me>
2020-08-04 23:30:14 +0200

    This bork prod

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -10,7 +10,7 @@ module.exports = (env, options) => ({
   optimization: {
     minimizer: [
       new TerserPlugin({ cache: true, parallel: true, sourceMap: false }),
-      new OptimizeCSSAssetsPlugin({})
+      //new OptimizeCSSAssetsPlugin({})
     ]
   },
   entry: {
